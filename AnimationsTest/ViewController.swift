7c58adb45c3bf5efcd7028efa7fdc2523d1b6e76
Ildar Zalyalov <ildarzalyalovmos@gmail.com>
2020-04-13 14:08:40 +0300

    Initial Commit

---
+++ AnimationsTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimationsTest
+//
+//  Created by Ильдар Залялов on 13.04.2020.
+//  Copyright © 2020 Ildar Zalyalov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
